e5805ab8225e06b2a0ff2ba00a2ac9dceef191b5
Tutoriais por Max Vianna <tutoriaismaxvianna@gmail.com>
2023-10-21 13:30:59 -0300

    Script SQL para criar tabelas da Base de Dados

---
+++ script.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `characteristics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `characteristic` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `characteristics_users` (
+  `characteristic_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
